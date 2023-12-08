6fd0f2737f088580a1e403347fcde53a07ed0edc
fajrianaNabila <145302935+fajrianaNabila@users.noreply.github.com>
2023-12-08 10:51:22 +0700

    master

---
+++ app/src/main/java/com/example/food/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.food
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    //    val navController = findNavController(R.id.nav_host_fragment_container)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container)as NavHostFragment
+
+        val navController = navHostFragment.navController
+        findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+            .setupWithNavController(navController)
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.RecipesFragment,
+                R.id.favoriteRecipesFragment,
+                R.id.UserProfileFragment,
+            )
+        )
+        //bottomNavigationView.setupWithNavController(navController)
+       // setupActionBarWithNavController(navController,appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()  || super.onSupportNavigateUp()
+    }
+
+}
